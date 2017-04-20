fcb1471a1a2a9de22b90afd3c7bc12dc12909964
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-04-20 19:09:06 +1200

    Hmmmm

--- doc/demo1d.py
+++ doc/demo1d.py
@@ -4,45 +4,50 @@ import numpy.random as rng
 from scipy.stats import norm as norm
 
 # 1D example
-L = np.linspace(-30.0, 30.0, 100001)
+L = np.linspace(-10.0, 10.0, 20001)
 dL = L[1] - L[0]
 
 # Prior
-pi = np.exp(-np.abs(L))
+pi = np.exp(-0.5*np.abs(L + 0.1)**2)
 pi /= np.trapz(pi, x=L)
 
-# Enclosed mass
-X = 1.0 - dL*np.cumsum(pi)
-
 # NS distribution
-pNS = np.zeros(len(L))
-pNS[X > 0.0] = pi[X > 0.0]/X[X > 0.0] # Avoid division by zero
-C = np.trapz(pNS, x=L)
-pNS /= C
+X = 1.0 - np.cumsum(pi)*dL
+print(X[0])
+pNS = pi/(X + 1E-300)
+pNS /= np.trapz(pNS, x=L)
 
-# Plot the prior and the NS distribution
-plt.plot(L, pi,  label=r"$\pi$")
-plt.plot(L, pNS, label=r"$p_{\rm NS}$")
-plt.legend()
-plt.xlabel(r"$L$")
-plt.ylabel("Probability Density")
-plt.show()
+# Return a constrained prior
+# and its X-value.
+def constrained_prior(ell):
+    p = pi.copy()
+    p[L <= ell] = 0.0
+    X = np.trapz(p, x=L)
+    p /= X
+    return {"p": p, "X": X}
 
-# Some thresholds to try
-thresholds = [-0.4, 0.7, 2.2]
-for thresh in thresholds:
-    # Constrained prior
-    constrained_prior = pi.copy()
-    constrained_prior[L <= thresh] = 0.0
-    X = np.trapz(constrained_prior, x=L)
-    constrained_prior /= X
-    KL = np.trapz(constrained_prior*\
-                  np.log(constrained_prior/(pNS + 1E-300) + 1E-300),
-                  x=L)
-    print(thresh, X, KL)
-    plt.plot(L, constrained_prior)
+# KL divergence
+def KL(p, q):
+    integrand = p*np.log(p/(q + 1E-300) + 1E-300)
+    return np.trapz(integrand, x=L)
 
-plt.xlabel(r"$L$")
-plt.ylabel("Probability Density")
+l1, l2 = 1.10, 1.15
+cp1 = constrained_prior(l1)
+cp2 = constrained_prior(l2)
+dl = l2 - l1
+KL1 = KL(cp1["p"], pNS)
+KL2 = KL(cp2["p"], pNS)
+print("X1 = {X}".format(X=cp1["X"]))
+print("X2 = {X}".format(X=cp2["X"]))
+print("")
+print("KL1 = {kl}".format(kl=KL1))
+print("KL2 = {kl}".format(kl=KL2))
+
+print("d(KL)/dl = {deriv}".format(deriv=(KL2 - KL1)/dl))
+
+plt.plot(L, pi)
+plt.plot(L, cp1["p"])
+plt.plot(L, cp2["p"])
+plt.plot(L, pNS)
 plt.show()
 
