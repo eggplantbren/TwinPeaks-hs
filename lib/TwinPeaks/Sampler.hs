5c6cd10a0aebe129611020c5f3ad41826f7cafae
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-09-17 12:31:41 +1200

    Working on Sampler type

--- lib/TwinPeaks/Sampler.hs
+++ lib/TwinPeaks/Sampler.hs
@@ -2,5 +2,35 @@
 
 module TwinPeaks.Sampler where
 
+-- Imports
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as U
+import TwinPeaks.Model
 
+-- A type that contains sampler options
+data SamplerOptions = SamplerOptions
+                      {
+                        -- Number of particles
+                        numParticles :: !Int,
+
+                        -- MCMC steps per level
+                        mcmcSteps    :: !Int,
+
+                        -- Backtracking length
+                        lambda       :: !Double
+                      }
+
+
+-- A type that represents a sampler's state
+data Sampler a = Sampler
+                 {
+                   -- The model specification
+                   theModel :: !(Model a),
+
+                   -- The particles! What else?
+                   theParticles :: !(V.Vector a),
+
+                   -- Particle indices
+                   particleIndices :: !(U.Vector (Int, Int))
+                 }
 
