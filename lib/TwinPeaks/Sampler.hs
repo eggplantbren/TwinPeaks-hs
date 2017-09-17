e6970e3a403acf89c1cbbc430ecd55861cb9f7ba
Brendon J. Brewer <brendon.brewer@gmail.com>
2017-09-17 12:32:31 +1200

    Sampler has options too

--- lib/TwinPeaks/Sampler.hs
+++ lib/TwinPeaks/Sampler.hs
@@ -8,17 +8,17 @@ import qualified Data.Vector.Unboxed as U
 import TwinPeaks.Model
 
 -- A type that contains sampler options
-data SamplerOptions = SamplerOptions
-                      {
-                        -- Number of particles
-                        numParticles :: !Int,
+data Options = Options
+               {
+                  -- Number of particles
+                  numParticles :: !Int,
 
-                        -- MCMC steps per level
-                        mcmcSteps    :: !Int,
+                  -- MCMC steps per level
+                  mcmcSteps    :: !Int,
 
-                        -- Backtracking length
-                        lambda       :: !Double
-                      }
+                  -- Backtracking length
+                  lambda       :: !Double
+               }
 
 
 -- A type that represents a sampler's state
@@ -26,6 +26,9 @@ data Sampler a = Sampler
                  {
                    -- The model specification
                    theModel :: !(Model a),
+
+                   -- The options
+                   options :: !Options,
 
                    -- The particles! What else?
                    theParticles :: !(V.Vector a),
